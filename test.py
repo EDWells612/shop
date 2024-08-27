7ddaf33304a8f4a4f85d19f16cbedda736f529a8
EDWells612 <edwells612@gmail.com>
2024-08-27 23:49:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,139 +1,120 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import ast
-from mlxtend.frequent_patterns import apriori, association_rules
+from utils import parse_items, plot_items_in_channel, plot_item_distribution, perform_apriori_analysis, get_items, get_list_items, get_items_input, update_csv
 
+# Load data
 data = pd.read_csv('shop.csv', parse_dates=['date'])
+itemsList = get_items(data)
+# Navigation
+page = st.sidebar.selectbox("Select Page", ["Home", "Channels", "Items", "New Entry"])
 
-def parse_items(item_str):
-    if pd.isna(item_str) or item_str == '':
-        return []
-    try:
-        item_list = ast.literal_eval(item_str)
-        if isinstance(item_list, list):
-            item_list = [i.strip().lower() for i in item_list if isinstance(i, str)]
-            return item_list
-    except (ValueError, SyntaxError):
-        return []
-    return []
+# Set default dates
+min_date = data['date'].min().date()
+max_date = data['date'].max().date()
 
-def plot_items_in_channel(data, channel):
-    filtered_channel_data = data[data['Channel'] == channel] if channel != 'All' else data
-    items_list = filtered_channel_data['items'].apply(parse_items).explode()
-    items_count = items_list.value_counts()
+# Date input
+start_date = st.sidebar.date_input('Start Date', min_value=min_date, max_value=max_date, value=min_date)
+end_date = st.sidebar.date_input('End Date', min_value=start_date, max_value=max_date, value=max_date)
 
-    if items_count.empty:
-        st.write(f"No items found in {channel}")
-        return
-    if channel != 'All':
-        fig, ax = plt.subplots(figsize=(10, 10))
-    else:
-        fig, ax = plt.subplots(figsize=(10, 20))
-    items_count.plot(kind='barh', ax=ax)
-    ax.set_xlabel('Count')
-    ax.set_ylabel('Items')
-    ax.set_title(f'Items bought in {channel}')
-    st.pyplot(fig)
+# Filter data by date range
+start_date = pd.to_datetime(start_date)
+end_date = pd.to_datetime(end_date)
+filtered_data = data[(data['date'].between(start_date, end_date))]
 
-def plot_channels_pie(data):
-    channel_counts = data['Channel'].value_counts()
-    fig, ax = plt.subplots()
-    ax.pie(channel_counts, labels=channel_counts.index, autopct='%1.1f%%', textprops={'fontsize': 5})
-
-    st.pyplot(fig)
-
-def plot_item_distribution(data, item):
-    filtered_data = data[data['items'].apply(lambda x: item in parse_items(x))]
-    item_distribution = filtered_data['Channel'].value_counts()
-    
-    fig, ax = plt.subplots(figsize=(10, 5))
-    item_distribution.plot(kind='bar', ax=ax)
-    ax.set_xlabel('Count')
-    ax.set_ylabel('Channels')
-    ax.set_title(f'Distribution of {item}')
-    st.pyplot(fig)
-
-def perform_apriori_analysis(data, min_support=0.1, metric="lift", min_threshold=1.0):
-    data['items_list'] = data['items'].apply(parse_items)
-    oht = data['items_list'].str.join('|').str.get_dummies()
-    
-    frequent_itemsets = apriori(oht, min_support=min_support, use_colnames=True)
-    rules = association_rules(frequent_itemsets, metric=metric, min_threshold=min_threshold)
-    
-    rules['antecedents'] = rules['antecedents'].apply(lambda x: ', '.join(list(x)))
-    rules['consequents'] = rules['consequents'].apply(lambda x: ', '.join(list(x)))
-    
-    return rules
-
-page = st.sidebar.selectbox("Select Page", ["Home", "Channels", "Items"])
-
+# Home page
 if page == "Home":
-    st.title("Farahy Shop Analysis")
-    st.write("Pie chart showing the distribution of channels.")
-    plot_channels_pie(data)
-
-elif page == "Channels":
-    channel = st.sidebar.selectbox('Select Channel', ['All'] + list(data['Channel'].unique()))
-    
-    min_date = data['date'].min().date()
-    max_date = data['date'].max().date()
-    
-    start_date = st.sidebar.date_input('Start Date', min_value=min_date, max_value=max_date, value=min_date)
-    end_date = st.sidebar.date_input('End Date', min_value=start_date, max_value=max_date, value=max_date)
-    
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
-    
-    if channel == 'All':
-        filtered_data = data[(data['date'].between(start_date, end_date))]
+    st.snow()
+    st.title("Farahy Shop")
+    search = st.text_input("Search", "")
+    if search:
+        filtered_data = filtered_data[filtered_data['items'].str.contains(search, case=False, na=False)]
+    if filtered_data.empty:
+        st.write("No results found.")
     else:
-        filtered_data = data[(data['Channel'] == channel) &
-                             (data['date'].between(start_date, end_date))]
+        n = 1
+        if (len(filtered_data) > 1):
+            n = st.slider('Number of rows to view', 1, len(filtered_data), len(filtered_data))
+        edited_data = st.data_editor(filtered_data.head(n),key = 'data_editor')
+        if st.button("Save Changes"):
+            # Update original data
+            data.update(edited_data)
+            update_csv(data)
+
+# Channels page
+elif page == "Channels":
+    channel = st.sidebar.selectbox('Select Channel', ['All'] + list(data['Channel'].unique()))    
+    if channel != 'All':
+        filtered_data = filtered_data[filtered_data['Channel'] == channel]
 
     if not filtered_data.empty:
         filtered_data.set_index('date', inplace=True)
-
-        time_series = filtered_data.resample('M').sum()
-
-        st.line_chart(time_series['Total Amount'])
-
-        st.write('Summary statistics for ', 'All' if channel == 'All' else channel)
+        st.line_chart(filtered_data['Total Amount'].resample('M').sum())
+        st.write(f'Summary statistics for {"All channels" if channel == "All" else channel}')
         st.write(filtered_data.describe())
-
         st.write(f'Items bought in {"all channels" if channel == "All" else channel}')
         plot_items_in_channel(filtered_data, channel)
 
         st.write(f"Apriori analysis for {'all channels' if channel == 'All' else channel}")
         rules = perform_apriori_analysis(filtered_data, min_support=0.1, metric="lift", min_threshold=1.0)
-        
         if not rules.empty:
             st.write(rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']])
         else:
             st.write("No significant rules found with the selected parameters.")
-
     else:
         st.write("No data available for the selected filters.")
 
+# Items page
 elif page == "Items":
     st.title("Items Analysis")
-    
-    item = st.sidebar.selectbox('Select Item', pd.Series(data['items'].apply(parse_items).explode()).unique())
-    
-    min_date = data['date'].min().date()
-    max_date = data['date'].max().date()
-    
-    start_date = st.sidebar.date_input('Start Date', min_value=min_date, max_value=max_date, value=min_date)
-    end_date = st.sidebar.date_input('End Date', min_value=start_date, max_value=max_date, value=max_date)
-    
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
-    
-    filtered_data = data[(data['date'].between(start_date, end_date)) & 
-                         (data['items'].apply(lambda x: item in parse_items(x)))]
-
-    if item and not filtered_data.empty:
+    item = st.sidebar.selectbox('Select Item', pd.Series(filtered_data['items'].apply(parse_items).explode()).unique())
+    if item:
         st.write(f'Distribution of {item}')
         plot_item_distribution(filtered_data, item)
-    else:
-        st.write("No data available for the selected filters.")
+# data entry page
+elif page == "New Entry":
+    st.title("New Entry")
+    with st.container():
+        col1, col2, col3, col4 = st.columns(4)
+        job_number = col1.number_input("Job Number", value=data['Job #'].max() + 1)
+        date = col2.date_input("Date", value=pd.to_datetime('today'))
+        cnls = data['Channel'].unique().tolist()
+        channel = col3.selectbox("Channel", ["New Channel"] + cnls, index=None)
+        if (channel == "New Channel"):
+            channel = col3.text_input("Channel", "")
+        with col4:
+            ad = st.checkbox("Ad", value=False)
+            rent = st.checkbox("Rent", value=False)
+    with st.container():
+        col1, col2, col3, col4 = st.columns(4)
+        amount = col1.number_input("Amount", value=0.0)
+        shipping = col2.number_input("Shipping", value=0.0)
+        paid_by = col3.number_input("Paid by Farahy", value=0.0)
+        farahy_income = col4.number_input("Farahy Income", value=0.0)
+    # Get items and amounts
+    with st.container():
+        items_str = get_items_input(itemsList)
+
+    if st.button("Submit Entry"):
+        new_entry = {
+            "Job #": job_number,
+            "date": pd.to_datetime(date),
+            "Channel": channel,
+            "Ad": (lambda x: 'Y' if x else 'N')(ad),
+            "rent": (lambda x: 'Y' if x else 'N')(rent),
+            "Amount": amount,
+            "Shipping": shipping,
+            "Total Amount": amount + shipping,
+            "Paid by F": paid_by,
+            "Farahy Income": farahy_income,
+            "Item": items_str,
+            "items" : get_list_items(items_str)
+        }
+        st.write(new_entry)
+        # Append new entry to the DataFrame (Replace with your actual data appending logic)
+        data = pd.concat([data, pd.DataFrame([new_entry])], ignore_index=True)
+        st.success("New entry added successfully!")
+        st.balloons()
+        st.write(data.tail(1))
+        data.to_csv('shop.csv', index=False)
+        # clear session state
+        st.session_state.clear()
